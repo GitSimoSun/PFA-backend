65a7a388a853230b7a18160489ef4b5f139c4109
Echchamsi Mohamed <echchamsi.mohamed1@gmail.com>
2021-06-16 01:48:03 +0100

    90% done

--- pfa/settings.py
+++ pfa/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pfa.urls'
@@ -135,6 +140,4 @@ CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
 ]
 
-# Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
